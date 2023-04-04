698f5b1d9a820085c00f51b599dd1839358d4bab
Lucian Iacob <lucian.i@rounds.com>
2023-04-04 13:21:36 +0300

    Updated copyright

--- app/src/main/java/com/farmexpert/android/activities/AuthenticationActivity.kt
+++ app/src/main/java/com/farmexpert/android/activities/AuthenticationActivity.kt
@@ -1,9 +1,9 @@
 /*
  * Developed by Lucian Iacob.
- * Cluj-Napoca, 2023.
+ * Romania, 2023.
  * Project: FarmExpert
  * Email: lucian@iacob.email
- * Last modified 4/4/23, 1:13 PM.
+ * Last modified 4/4/23, 1:22 PM.
  * Copyright (c) Lucian Iacob. All rights reserved.
  */
 
